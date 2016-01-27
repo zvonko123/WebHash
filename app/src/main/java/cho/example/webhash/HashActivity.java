1a524c5f5a364e902764284e4b025ced1bfbc935
Zvonimir Štimac <zvone@mbit.io>
2016-01-27 19:38:11 +0100

    apache ioUtils added. we need async network task

--- app/src/main/java/cho/example/webhash/HashActivity.java
+++ app/src/main/java/cho/example/webhash/HashActivity.java
@@ -1,5 +1,6 @@
 package cho.example.webhash;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,9 +9,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.commons.io.IOUtils;
 
 import java.io.BufferedInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -18,11 +25,14 @@ import java.net.URL;
 
 public class HashActivity extends ActionBarActivity {
 
+    Context mContext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hash);
 
+        mContext = this;
         /*For example, to retrieve the webpage at http://www.android.com/:
         URL url = new URL("http://www.android.com/");
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
@@ -42,10 +52,19 @@ public class HashActivity extends ActionBarActivity {
                     URL uri = new URL(uriInputBox.getText().toString());
                     HttpURLConnection urlConnection = (HttpURLConnection) uri.openConnection();
                     InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                    
+                    Writer out = new StringWriter();
+                    IOUtils.copy(in, out);
+
+                    ((TextView) findViewById(R.id.outputTextView)).setText(out.toString());
+
+
+                    //use multicatch ?
                 } catch (MalformedURLException e) {
                     e.printStackTrace();
-
+                    Toast.makeText(mContext, mContext.getString(R.string.malformed_url), Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(mContext, mContext.getString(R.string.io_exception), Toast.LENGTH_SHORT).show();
                 }
 
 
